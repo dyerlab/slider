d913f70180153b2d8107f396b3b4030dfa56e152
Rodney <rjdyer@vcu.edu>
2021-04-23 06:58:30 -0400

    initial commit

---
+++ Slider/SliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderApp.swift
+//  Slider
+//
+//  Created by Rodney Dyer on 4/23/21.
+//
+
+import SwiftUI
+
+@main
+struct SliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
